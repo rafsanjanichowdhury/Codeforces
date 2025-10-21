47a1977b95d140208b95764b62fd45820b2df7cf
rafsunjonychowdhury <rafsan.buet.10@gmail.com>
2025-10-22 00:49:17 +0600

    22 Oct, 2025

--- CF-31-800A.cpp
+++ CF-31-800A.cpp
@@ -1,6 +1,9 @@
 #include <iostream> // Includes the iostream library for input/output operations
 
+using namespace std;
+
 int main() { // The main function, where program execution begins
     std::cout << "Hello, World!" << std::endl; // Prints "Hello, World!" to the console
+	for(int i=0; i<100; i++) cout<<"Printing for loop" <<endl;
     return 0; // Indicates successful program execution
 }
